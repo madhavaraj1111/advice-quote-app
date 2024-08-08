0490f550b5115b9a7929156041f2b13d86d1ba68
Maddy <madhavaraj1111@gmail.com>
2024-08-08 14:52:47 +0530

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,32 @@
-// Write your javascript code here
+// https://api.adviceslip.com/advice
+
+const url = "https://api.adviceslip.com/advice";
+const quoteEl = document.querySelector("#quote");
+const btnEl = document.querySelector("#buttonQuote");
+const divEl=document.querySelector("quoteDiv");
+
+
+btnEl.addEventListener("click", () => {
+ window.location.reload();
+});
+
+async function adviceData() {
+  try {
+    const apiFetch = await fetch(url);
+    const jsonData = await apiFetch.json();
+    return jsonData;
+  } catch (error) {
+    console.log("Error caused !!!!", error);
+  }
+}
+
+adviceData().then((data) => {
+  if (data && data.slip) {
+    quoteEl.innerText = data.slip.advice;
+    console.log(data.slip.advice);
+  } else {
+    console.error("No advice found in the response.");
+  }
+});
+
+
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  // base: '/YOUR_BASE/'
+  base: '/advice-quote-app/'
   //   build: {
   //     rollupOptions: {
   //       input: {
